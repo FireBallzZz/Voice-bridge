69644dc8d790a816948a307b4060743c2cab8ffd
FireBallzZz <shrajon10@gmail.com>
2025-09-15 15:07:19 +0600

    About section page design

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
 from issues.forms import IssueForm
 from .forms import CustomUserCreationForm, LoginForm, EditProfileForm
 from issues.models import Issue
